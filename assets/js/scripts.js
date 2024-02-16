8922789deae6471e0e918a6d55f40f8b574108db
SaraNiknamHesar <saraniknaam76@gmail.com>
2024-02-16 20:31:51 +0100

    Remove_and_add_menu_using_buttons_with_JS

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+document.querySelector('.close').addEventListener('click', function (event) {
+    event.preventDefault();
+    document.querySelector('.header-top').classList.add('active');
+    setTimeout(function () {
+        showTopBar();
+    }, 800)
+})
+
+function showTopBar() {
+    document.querySelector('.showTopBar').classList.remove('d-none');
+}
+
+document.querySelector('.showTopBar').addEventListener('click', function (event) {
+    event.preventDefault();
+    document.querySelector('.header-top').classList.remove('active');
+    document.querySelector('.showTopBar').classList.add('d-none');
+})
