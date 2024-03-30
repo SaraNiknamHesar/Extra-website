901f9ada3b285d280a993c99afba1c8d802f4dda
SaraNiknamHesar <saraniknaam76@gmail.com>
2024-03-30 13:46:19 +0100

    implementing_website_with_flex_box

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,19 @@
-document.querySelector('.close').addEventListener('click', function (event) {
-    event.preventDefault();
-    document.querySelector('.header-top').classList.add('active');
-    setTimeout(function () {
-        showTopBar();
-    }, 800)
-})
+// document.querySelector('.close').addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('.header-top').classList.add('active');
+//     setTimeout(function () {
+//         showTopBar();
+//     }, 800)
+// })
 
-function showTopBar() {
-    document.querySelector('.showTopBar').classList.remove('d-none');
-}
+// function showTopBar() {
+//     document.querySelector('.showTopBar').classList.remove('d-none');
+// }
 
-document.querySelector('.showTopBar').addEventListener('click', function (event) {
-    event.preventDefault();
-    document.querySelector('.header-top').classList.remove('active');
-    document.querySelector('.showTopBar').classList.add('d-none');
-})
+// document.querySelector('.showTopBar').addEventListener('click', function (event) {
+//     event.preventDefault();
+//     document.querySelector('.header-top').classList.remove('active');
+//     document.querySelector('.showTopBar').classList.add('d-none');
+// })
+
+
