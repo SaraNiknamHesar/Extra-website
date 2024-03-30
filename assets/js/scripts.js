5adcadde44c968d18e9c0a819adbf4ca0bd19e22
SaraNiknamHesar <saraniknaam76@gmail.com>
2024-03-30 16:12:28 +0100

    drop_down_menu_header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,28 +1,14 @@
-// document.querySelector('.close-col').addEventListener('click', function (event) {
-//     event.preventDefault();
-//     document.querySelector('.top-bar').classList.add('active');
-//     setTimeout(function () {
-//         showTopBar();
-//     }, 800)
-// })
-
-// function showTopBar() {
-//     document.querySelector('.showTopBar').classList.remove('d-none');
-// }
-
-// document.querySelector('.showTopBar').addEventListener('click', function (event) {
-//     event.preventDefault();
-//     document.querySelector('.header-top').classList.remove('active');
-//     document.querySelector('.showTopBar').classList.add('d-none');
-// })
-document.querySelector('.close-col').addEventListener('click', (event) => {
+document.querySelector('.top-bar').addEventListener('click', (event) => {
     event.preventDefault();
     document.querySelector('.top-bar').classList.add('close-top-bar');
     setTimeout(() => {
         showTopBar();
     }, 800)
-    function showTopBar() {
-        document.querySelector('')
-    }
 });
-
+function showTopBar() {
+    document.querySelector('.showTopBar').classList.add('show-close-btn');
+}
+document.querySelector('.showTopBar').addEventListener('click', (event) => {
+    event.currentTarget.classList.remove('show-close-btn');
+    document.querySelector('.top-bar').classList.remove('close-top-bar')
+});
